62fb5fda50a2a880e99446c32342be26b6528710
michelecobelli <michele.cobelli@students.iaac.net>
2025-05-26 17:42:55 +0200

    Fix CORS config to support OPTIONS preflight from Lovable

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 # 🔐 CORS setup for Lovable
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
